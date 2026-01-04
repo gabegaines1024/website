1a5d18835c29906d55360ff92d68dc3c83641698
Gabe <gabegaines1024@gmail.com>
2026-01-04 05:27:05 -0600

    small change to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
+import { Separator } from '@/components/ui/separator';
 import Home from './about/page';
 import ProjectCard from './components/ProjectCard';
 import { selfProjects } from './data/ResumeData';
+import Section from './components/Section';
 
 
 export default function App(){
   return (
     <div className="flex flex-col gap-4">
       <Home />
-      <div className="flex flex-col gap-4">
-        {selfProjects.map((project, index) => (
-          <ProjectCard key={project.id} project={project} />
-        ))}
-      </div>
+      <Section title="Projects">
+        <div className="flex flex-col gap-4">
+          {selfProjects.map((project, index) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </div>
+      </Section>
     </div>
   )
 }
