14f1743d5015fe331ffe3e2ab3aadc9f72a6795c
Gabe <gabegaines1024@gmail.com>
2026-01-05 14:27:37 -0600

    lighter theme and centered cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,23 @@ import ProjectCard from './components/ProjectCard';
 import { selfProjects, selfResumeData } from './data/ResumeData';
 import Section from './components/Section';
 import Header from './components/Header';
+import './globals.css';
 
 
 export default function App(){
   return (
-    <div className="flex flex-col gap-4">
-      <Header data={selfResumeData} />
-      <Section title="Projects">
-        <div className="flex flex-col gap-4">
-          {selfProjects.map((project, index) => (
-            <ProjectCard key={project.id} project={project} />
-          ))}
-        </div>
-      </Section>
-      <Separator className="my-4" />
+    <div className="max-w-4xl mx-auto">
+      <div className="flex flex-col gap-4">
+        <Header data={selfResumeData} />
+        <Section title="Projects">
+          <div className="flex flex-col gap-4">
+            {selfProjects.map((project, index) => (
+              <ProjectCard key={project.id} project={project} />
+            ))}
+          </div>
+        </Section>
+        <Separator className="my-4" />
+      </div>
     </div>
   )
 }
