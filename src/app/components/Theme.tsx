2174623b6315c87958871515c9bee6bb4810ffb3
Gabe <gabegaines1024@gmail.com>
2026-01-09 09:59:55 -0600

    changed the name of the component

---
+++ src/app/components/Theme.tsx
@@ -0,0 +1,7 @@
+import { useEffect, useState } from "react";
+import { Moon, Sun } from "lucide-react";
+
+export default function LightDarkMode(){
+    const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+
+}
